9224bcdc0063908d6113db3dd8fc6f8b0cb98d23
erickkirui <erickkirui653@gmail.com>
2024-06-09 21:29:19 +0300

    completed basic requirements

--- frontend/src/Components/BookList.js
+++ frontend/src/Components/BookList.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { gql } from '@apollo/client';
+import { Alert } from '@mui/material';
 import BookCard from './SingleBookCard';
 import './Styles/BookList.css';
 
@@ -19,6 +20,7 @@ const BookList = ({ addToRead }) => {
   const { loading, error, data } = useQuery(GET_BOOKS);
   const [currentPage, setCurrentPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState('');
+  const [showAlert, setShowAlert] = useState(false); // State to control alert visibility
   const booksPerPage = 9;
 
   if (loading) return <p>Loading...</p>;
@@ -35,6 +37,18 @@ const BookList = ({ addToRead }) => {
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+  const handleAddToList = (book) => {
+    // Perform any necessary actions when adding to the list
+    // Show the alert
+    setShowAlert(true);
+    // Add the book to the reading list
+    addToRead(book);
+    // Hide the alert after 3 seconds
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 1500);
+  };
+
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(filteredBooks.length / booksPerPage); i++) {
     pageNumbers.push(i);
@@ -42,6 +56,7 @@ const BookList = ({ addToRead }) => {
 
   return (
     <div>
+      {/* Search input */}
       <input
         type="text"
         placeholder="I am looking for ..."
@@ -49,6 +64,17 @@ const BookList = ({ addToRead }) => {
         onChange={(e) => setSearchTerm(e.target.value)}
         className="search-input"
       />
+      
+      <div style={{ marginTop: '10px' }}>
+        {/* Material UI Alert */}
+        {showAlert && (
+          <Alert variant="filled" severity="success">
+            Book Added to your Reading list
+          </Alert>
+        )}
+      </div>
+      
+      {/* Book list */}
       <div className="book-list">
         {currentBooks.map((book, index) => (
           <BookCard
@@ -57,10 +83,12 @@ const BookList = ({ addToRead }) => {
             author={book.author}
             coverPhotoURL={book.coverPhotoURL}
             readingLevel={book.readingLevel}
-            onAddToRead={() => addToRead(book)}
+            onAddToRead={() => handleAddToList(book)} // Modified to call handleAddToList
           />
         ))}
       </div>
+
+      {/* Pagination */}
       <div className="pagination">
         {pageNumbers.map((number) => (
           <button key={number} onClick={() => paginate(number)} className="page-number">
